ae6dfb82a78e6c576c86932493b0452a48b3754b
AQUAA <165506354+AAquaas@users.noreply.github.com>
2025-06-17 13:52:35 +0100

    Add files via upload
    Added all pages as a template to be completed. the nav bar runs as intended, but shows Coffee.co - Account instead of Coffee.co - Location on the location page

--- Flask.py
+++ Flask.py
@@ -6,28 +6,28 @@ app.config['DEBUG'] = True
 
 
 
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-@app.route('/hello')
-def hello():
-    message = "<h1>Here's a random number: {0}</h1> <h2>hhh</h2><h3>hi </h3> <p>hello</p>"
-    num = random.randint(1, 25)
-    return render_template("random.html")
-
-@app.route('/goodbye')
-def goodbye():
-   message = "<h2>This is the second page!</h2>"
-   return message
-
-@app.route('/thanks')
-def thanks():
-        person = "Bob"
-        action = "reading"
-        end = "Thanks"
-        writer = "Ryan"
-        return render_template("tynote.html", name=person, verb=action, gift="book", noun="person", closing_word=end, author=writer)
+# @app.route('/')
+# def index():
+#     return render_template("index.html")
+#
+# @app.route('/hello')
+# def hello():
+#     message = "<h1>Here's a random number: {0}</h1> <h2>hhh</h2><h3>hi </h3> <p>hello</p>"
+#     num = random.randint(1, 25)
+#     return render_template("random.html")
+#
+# @app.route('/goodbye')
+# def goodbye():
+#    message = "<h2>This is the second page!</h2>"
+#    return message
+#
+# @app.route('/thanks')
+# def thanks():
+#         person = "Bob"
+#         action = "reading"
+#         end = "Thanks"
+#         writer = "Ryan"
+#         return render_template("tynote.html", name=person, verb=action, gift="book", noun="person", closing_word=end, author=writer)
 
 
 @app.route('/coffee.co/home')
@@ -39,6 +39,26 @@ def home_page():
 
 def order_page():
     return render_template("coffeeco_order.html")
+
+@app.route('/coffee.co/menu')
+
+def menu_page():
+    return render_template("coffeeco_menu.html")
+
+@app.route('/coffee.co/account')
+
+def account_page():
+    return render_template("coffeeco_account.html")
+
+@app.route('/coffee.co/rewards')
+
+def rewards_page():
+    return render_template("coffeeco_account.html")
+
+@app.route('/coffee.co/locations')
+
+def locations_page():
+    return render_template("coffeeco_account.html")
 
 if __name__ == '__main__':
     app.run()
