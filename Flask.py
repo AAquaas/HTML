d72a930606111c11a8d8b8f8ad37bcee5592dae7
AQUAA <165506354+AAquaas@users.noreply.github.com>
2025-06-24 15:40:50 +0100

    Add files via upload

--- Flask.py
+++ Flask.py
@@ -1,20 +1,20 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 import random
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
-
 @app.route('/')
 def index():
     return render_template("index.html")
 
- # @app.route('/hello')
- # def hello():
- #     message = "<h1>Here's a random number: {0}</h1> <h2>hhh</h2><h3>hi </h3> <p>hello</p>"
- #     num = random.randint(1, 25)
- #     return render_template("random.html")
+
+# @app.route('/hello')
+# def hello():
+#     message = "<h1>Here's a random number: {0}</h1> <h2>hhh</h2><h3>hi </h3> <p>hello</p>"
+#     num = random.randint(1, 25)
+#     return render_template("random.html")
 #
 # @app.route('/goodbye')
 # def goodbye():
@@ -32,7 +32,8 @@ def index():
 
 @app.route('/coffee.co/home')
 def home_page():
-    return render_template("coffeeco_home.html")
+    return render_template("coffeeco_home.html", subtitle="Home")
+
 
 @app.route('/coffee.co/order', methods=('GET', 'POST'))
 def order_page():
@@ -44,26 +45,28 @@ def order_page():
 
         return render_template("coffeeco_result.html", drink=drink, milk=milk, size=size, ice=ice)
 
-    return render_template("coffeeco_order.html")
+    return render_template("coffeeco_order.html", subtitle="Order")
 
 
 @app.route('/coffee.co/menu')
 def menu_page():
-    return render_template("coffeeco_menu.html")
+    return render_template("coffeeco_menu.html", subtitle="Menu")
+
 
 @app.route('/coffee.co/account')
 def account_page():
-    return render_template("coffeeco_account.html")
+    return render_template("coffeeco_account.html", subtitle="Account")
+
 
 @app.route('/coffee.co/rewards')
 def rewards_page():
     return render_template("coffeeco_rewards.html")
 
+
 @app.route('/coffee.co/locations')
 def locations_page():
-    return render_template("coffeeco_locations.html")
+    return render_template("coffeeco_locations.html", subtitle="Location")
+
 
 if __name__ == '__main__':
     app.run()
-
-
